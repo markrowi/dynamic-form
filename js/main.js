a25400abae54dbfe469c689e9e66f79f37249519
Rowi Dizon <rowi.dizon@nuworks.ph>
2017-10-24 18:36:57 +0800

    main

--- js/main.js
+++ js/main.js
@@ -1,53 +1,17 @@
 $(document).ready(function(){
     const form_content = webData.form_content
-    let $forms = [];
-    let $wizards = [];
+    
 
     var wizz = new WizardManager(form_content);        
     wizz.init();
     
-
 });
 
 
 
-    function Wizard(wz){
-        this.name = wz.name; 
-        this.fields = wz.fields;
-    }
-
-    Wizard.prototype.render = function render (btn) {
-    $content = $(`<div class="container-fluid wizard" data-wizard=${this.name}>
-                <div class="row actions action-top"></div>
-                <div class="row form-content"><h1>${this.name}</h1></div>
-                <div class="row actions action-bottom"></div>
-            </div>`)
- 
-
-    $.each(btn.top, function(i, btn){
-        $content.find('.action-top').append(actionTemplate(btn))
-    })
-    $.each(btn.bottom, function(i, btn){
-        $content.find('.action-bottom').append(actionTemplate(btn))
-    })
-
-    var form = new Form(this.fields);
-
-    $content.find('.form-content').append(form.render())
-
-    return $content;
-}
-
-// Form.prototype.nextWizard = function nextWizard(){
-//     // this.$content.data('index')
-// }
+  
 
 
-function actionTemplate(btn){
-    return `<div class="col-md-12">
-            <input type="button" name="" data-action="${btn.action}" class="btn btn-primary form-control" id="" value="${btn.label}">
-        </div>`
-}
 
 
 
