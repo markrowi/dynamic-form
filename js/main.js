1365754c9bb15f49f8c2bc8ed467b083383a6dc3
= <=>
2017-10-25 21:15:00 +0800

    main

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     
     $.get('https://events.enlo.digital/api/event/5', function(res){
         res.form_content = JSON.parse(res.form_content);
-
+    
         var wizz = new WizardManager(res.form_content);        
         wizz.init();
     })
